5b19d04e1d8a968b6946a98a786c91eacac3bcff
Annisa Dian Amarta <anisadiana525@gmail.com>
2020-10-26 18:49:26 +0700

    MVC rental kendaraan selesai

--- RentalKendaraan_062/Startup.cs
+++ RentalKendaraan_062/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -30,6 +31,8 @@ namespace RentalKendaraan_062
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddDbContext<Models.RentalkendaraanContext>(options =>options.UseSqlServer(Configuration.GetConnectionString("Default")));
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
